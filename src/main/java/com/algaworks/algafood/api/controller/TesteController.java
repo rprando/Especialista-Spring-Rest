340d02fdd902c0b9ba5a003dc405da050ae957a6
Rodrigo Prando <ralmeida@fh.com.br>
2020-06-29 16:17:47 -0300

    Externalizando consultas JPQL para um arquivo XML

---
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+@RestController
+@RequestMapping("/testes")
+public class TesteController {
+	
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
+	@GetMapping("/restaurantes/por-nome")
+	public List<Restaurante> restaurantePorNome(String nome, Long cozinhaId){
+		return restauranteRepository.consultarPorNome(nome, cozinhaId);
+	}
+
+}
